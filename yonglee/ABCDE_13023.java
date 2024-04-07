52927e78e1accb21f5f9e04e1ab3bf030c7b4f21
usingjun <97494494+usingjun@users.noreply.github.com>
2024-04-07 23:56:02 +0900

    Update ABCDE_13023.java

--- yonglee/ABCDE_13023.java
+++ yonglee/ABCDE_13023.java
@@ -1,11 +1,15 @@
+/*
+-> DFS문제, 특정 깊이에 도달하면 정답
+*/
+
 import java.util.*;
 import java.io.*;
 
 public class Main {
 
     static ArrayList<Integer> A[];
-    static boolean[] visited;
-    static boolean counted;
+    static boolean[] visited; //도착했는지
+    static boolean counted; //정답인지
 
 
     public static void main(String[] args) throws Exception{
@@ -29,13 +33,13 @@ public class Main {
             int e = Integer.parseInt(st.nextToken());
 
             A[s].add(e);
-            A[e].add(s);
+            A[e].add(s); 
         }
 
         for (int i = 0; i < n; i++) {
-            if (!visited[i]) {
-                DFS(i,0);
-                if (counted) {
+            if (!visited[i]) {//여기까지 똑같은 DFS
+                DFS(i,0);  //깊이를 통해 정답을 추론해야 하기때문에 깊이 매개변수 추가
+                if (counted) { //만약 counted가 충족한다면 break
                     break;
                 }
             }
@@ -49,19 +53,19 @@ public class Main {
     }
 
     static void DFS(int n, int cnt) {
-        if (cnt == 4) {
+        if (cnt == 4) { //깊이에 도달한다면 return
             counted = true;
             return;
         }
         if (visited[n]) {
             return;
         }
-        visited[n] = true;
+        visited[n] = true; //방문했는지 체크
         for(int i :A[n]){
             if (!visited[i]) {
                 DFS(i,cnt+1);
             }
         }
-        visited[n] = false;
+        visited[n] = false;// 방문한 곳 다시 되돌리기
     }
 }
